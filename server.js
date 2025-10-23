4f6dbec788f20bbd15edd84ab7784aced69425f7
Deion O'Garro <deionog@gmail.com>
2025-10-22 21:53:18 -0400

    feat: Initialize fastify project (#1)
    * Run Fastify with Typescript
    * Setup both index.ts and server.js files to experiment with both
    * Add potential mocks
    * Add git ignore: ignore generated files and node modules
    * Add a routes direct to explore writing plugins ouside main server files

---
+++ server.js
@@ -0,0 +1,36 @@
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+  return { hello: 'world' }
+})
+
+fastify.route({
+  method: 'GET',
+  url: '/route-options',
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: { type: 'string' }
+        }
+      }
+    }
+  },
+  handler: async (request, reply) => {
+    return { hello: 'world' }
+  }
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
